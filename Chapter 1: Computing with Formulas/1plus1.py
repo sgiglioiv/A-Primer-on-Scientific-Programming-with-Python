bb4e3b7933eceb3c6b84f312932dbb9112da17c0
sgiglioiv <51755823+sgiglioiv@users.noreply.github.com>
2019-12-12 15:20:19 -0600

    Add files via upload

---
+++ Chapter 1: Computing with Formulas/1plus1.py
@@ -0,0 +1,11 @@
+#Exercise 1.1
+#Author: Samuel Giglio IV
+
+answer = 1 + 1
+print(answer)
+
+"""
+Sample run:
+python3 1plus1.py
+2
+"""
---
+++ Chapter 1: Computing with Formulas/hello_world.py
@@ -0,0 +1,10 @@
+#Exercise 1.2 Write a "Hello, World!" program.
+#Author: Samuel Gilio IV
+
+print("Hello, World!")
+
+
+"""
+Sample run
+"Hello, World!"
+"""
